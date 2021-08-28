e4b2af62769b34c12b41067a72dc8ee130ea5b44
vineeshbunny <vmvr2799@gmail.com>
2021-08-28 18:27:35 +0530

    28/8/21

--- login/main/main.js
+++ login/main/main.js
@@ -2,7 +2,20 @@ let num=Math.floor(Math.random() * 100);
 
 const quotesy= document.querySelector(".quote")
 const author= document.querySelector(".author")
+const left= document.querySelector(".left")
+const right= document.querySelector(".right")
 
+
+
+left.addEventListener("click",()=>{
+    left.style.opacity=0
+    right.style.opacity=1
+})
+
+right.addEventListener("click",()=>{
+    right.style.opacity=0
+    left.style.opacity=1
+})
 
 
 async function jokes(){
