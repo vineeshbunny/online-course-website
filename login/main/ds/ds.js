9bb8d7ec1dce9cd3dcd86e0a594416c2be9110bb
vineeshbunny <vmvr2799@gmail.com>
2021-09-03 10:27:30 +0530

    final

---
+++ login/main/ds/ds.js
@@ -0,0 +1,45 @@
+const overview= document.querySelector(".overview1")
+const week1= document.querySelector(".week1")
+const week2= document.querySelector(".week2")
+const right=document.querySelector(".right")
+const starts=document.querySelector(".starts")
+week1.addEventListener("click",(e)=>{
+        e.preventDefault()
+      
+
+        
+        right.style.top="-90vh"
+
+
+       
+            
+        
+
+
+
+})
+week2.addEventListener("click",(e)=>{
+        e.preventDefault()
+        right.style.top="-180vh"
+        
+
+
+
+})
+starts.addEventListener("click",(e)=>{
+        e.preventDefault()
+        right.style.top="-90vh"
+        
+
+
+
+})
+
+
+
+
+
+
+
+
+
